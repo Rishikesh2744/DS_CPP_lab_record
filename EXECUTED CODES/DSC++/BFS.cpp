9ff99c9864a1a676ed1eb13a30e39e521f626487
alien <98737806+Rishikesh2744@users.noreply.github.com>
2023-03-06 10:42:15 +0530

    Update BFS.cpp

--- EXECUTED CODES/DSC++/BFS.cpp
+++ EXECUTED CODES/DSC++/BFS.cpp
@@ -1,45 +1,64 @@
 #include <iostream>
 using namespace std;
-int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
-void bfs(int v)
+
+class bfs(int S)
 {
-    visited[v] = 1;
-    cout << v << "\t";
-    while (1)
-    {
-        for (i = 1; i <= n; i++) // for all the vertices
+        visited[S]=1;   //source is visited
+
+        cout<< S <<"\t";    //source is printed 
+
+        while(true)
         {
-            if (a[v][i] != 0 && visited[i] == 0) // adjacent to v and not visited
-            {
-                visited[i] = 1;
-                q[++r] = i;
-            }
+                for(int i=1;i<=n;i++)   
+                {
+                        if(graph[S][i]!=0 && visited[i]==0) // if adjacent to S and not visited or not
+                        {
+                                visited[i]=1;
+                                queue[++rear]
+                        }
+                }
+
+                if(front > rear)        //if front is greater than rear queue is empty
+                        break;
+                S=queue[front++];   // pop front vertex from queue
+                cout<< S << "\t';
         }
-        // if queue is empty, stop while loop
-        if (f > r)
-            break;
-        v = q[f++]; // pop front vertex from queue
-        cout << v << "\t";
-    }
 }
+
 int main()
 {
-    int v;
-    cout << "\n Enter the number of vertices:";
-    cin >> n;
-    for (i = 1; i <= n; i++)
-    {
-        visited[i] = 0;
-    }
-    cout << "\n Enter graph data in matrix form:\n";
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            cin >> a[i][j];
-    cout << "\n Enter the starting vertex:";
-    cin >> v;
-    cout << "BREADTH FIRST TRAVERSAL:\n";
-    bfs(v);
+        int S;
+
+        cout<<"\n enter number of vertices ";
+        cin>>n;
+    
+        int front=-1;rear=-1;
+        int queue[n] , graph[n][n] ,  visited[n];
+       
+
+        for(int i=1;i<=n;i++)
+        {
+                visited[i]=o;
+        }
+
+        cout<<"\n Enter graph data in matrix form";
+        for(int i=1;i<=n;i++)
+        {
+                for(int j=1;j<=n;j++;
+                {
+                        cin>>arr[i][j];
+                }
+        }
+
+        cout<<"\n Enter source vertex: ";
+        cin>>S;
+        cout<<" Breadth First Traversal : \n";
+
+        bfs(S);
+
+        return 0;
 }
+
 /*
 Output:
 Enter the number of vertices:5
@@ -52,3 +71,5 @@ Enter graph data in matrix form:
 Enter the starting vertex:3
 BREADTH FIRST TRAVERSAL:
 3 1 2 4 5*/
+
+                                                                                                                               27,1           6%
